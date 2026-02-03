0e2eb4fc13b756cf1298c2fbeed0c60d96d5bf57
gulvekartik45 <kartikgulve2002@gmail.com>
2026-02-03 16:31:20 +0530

    use redit

--- client/ecomm_frontend/src/pages/Admin/AdminDashboard.jsx
+++ client/ecomm_frontend/src/pages/Admin/AdminDashboard.jsx
@@ -150,7 +150,7 @@ const AdminDashboard = () => {
     setUserForm({ ...userForm, [e.target.name]: e.target.value });
 
   const handleEditUser = (u) => {
-    setEditUserId(u.id);
+    setEditUserId(u._id);
     setUserForm({
       firstName: u.firstName,
       lastName: u.lastName,
